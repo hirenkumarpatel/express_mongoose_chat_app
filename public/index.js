91b050ab4bde83ef552cac87b3355513753b91dd
Hiren Patel <hirenkumarbpatel@gmail.com>
2019-09-07 18:17:49 -0400

    old UI chat finished

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
+//created instnce of io()that will be passed to check new connection
+var socket=io();
 $(()=>{
+    
     //variable initialization
     const messageList=$(`#message-list`);
     const sendButton=$(`#sendButton`);
@@ -10,19 +13,36 @@ $(()=>{
     let addMessage=(data)=>{
     messageList.append(`<h4>${data.name}:</h4><p>${data.message}</p>`);
     }
+    //handling triggred event and create new message
+    socket.on("message",addMessage);
     
     //function to send message to message-list
     sendButton.on('click',()=>{
-       
-        addMessage({name:nameInput.val(),message:messageInput.val()});
+        var data={name:nameInput.val(),message:messageInput.val()}
+        postMessages(data);
     });
     let getMessages=()=>{
-        fetch("/messages",(data)=>{
-            data.forEach(addMessage);
-        }).then((res)=>{
+        fetch("/messages")
+        .then((res)=>{
             return res.json();
         }).then((data)=>{
-            console.log(JSON.stringify(data));
+            data.forEach(addMessage);
+        });
+    }
+    //get messages
+    getMessages();
+    let postMessages=(data)=>{
+        fetch("/messages",{
+            method:"POST",
+            headers:{"Content-Type":"application/json;charset=utf-8"},
+            body:JSON.stringify(data)
+        })
+        .then((res)=>{
+            return res.json()
+        }).then((data)=>{
+            addMessage(data);
+        }).catch(err=>{
+            console.log("Error in new message post");
         });
     }
 });
--- server.js
+++ server.js
@@ -1,35 +1,85 @@
 //importing express
-const express=require("express");
+const express = require("express");
 //importing body-parser to parse object in json data
-const bodyParser=require("body-parser"); 
+const bodyParser = require("body-parser");
 //creating instance of express
-const app=express();
+const app = express();
 //importing path to access file directories and its path functions
-const path=require("path");
-const port=process.env.PORT ||3000;
+const path = require("path");
+const port = process.env.PORT || 3000;
+//in order to make soket.io workd we have to include node first and then can connect it to express and socket
+//this is node's module
+const http = require("http").Server(app);
+//imported socket and linked with node
+const io = require("socket.io")(http);
+//importing mongoose database
+const mongoose = require("mongoose");
+//url of database and credential
+const mongooseURL =
+  "mongodb+srv://hirenpatel:hirenpatel@chatapp-gssy1.mongodb.net/test?retryWrites=true&w=majority";
+const mongooseOptions = { useNewUrlParser: true };
+// to include promise library to avoid nested callback hell
+mongoose.Promise = Promise;
 //Middleware function to make static files availbale to web server
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//defining model for mongoose schema
+const Message = mongoose.model("Message", {
+  name: String,
+  message: String,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
 //defining message json value as if we are importing from database
-let messages=[
-    {name:"Hiren",message:"Hii"},
-    {name:"Jane",message:"Hello"},
-    {name:"Hiren",message:"How are you?"}
+let messages = [
+  { name: "Hiren", message: "Hii" },
+  { name: "Jane", message: "Hello" },
+  { name: "Hiren", message: "How are you?" }
 ];
 
 //default layout
-app.get("/",(req,res)=>{
-    res.send(path.join(__dirname,'/public','/index.html'));
+app.get("/", (req, res) => {
+  res.send(path.join(__dirname, "/public", "/index.html"));
 });
 
 //messages json file
-app.get("/messages",(req,res)=>{
-    res.send(messages);
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, message) => {
+    res.json(message);
+  });
 });
 //post new message
-app.post("/messages",(req,res)=>{
-    messages.push(req.body);
-    res.status(200);
+app.post("/messages", async (req, res) => {
+  try {
+    //creating the object of mongoose's Datamodel Message and pass new  value
+    let message = new Message(req.body);
+    let savedMessage = await message.save();
+    let censored = await Message.findOne({ message: "badword" });
+    if (censored) await Message.deleteOne({ _id: censored.id });
+    //triggring custom event handling to notify all connected user about new message notification
+    else io.emit("message", req.body);
+    res.sendStatus(200);
+    res.json(JSON.stringify(req.body));
+  } catch (error) {
+     res.sendStatus(500);
+     console.log(`error ${error}`)
+  }
 });
-app.listen(port,()=>{
-    console.log(`server is running on port :${port}`);
+
+//handling io on new connection event
+io.on("connection", socket => {
+  console.log("new user connected!!");
+});
+//connecting to database
+mongoose.connect(mongooseURL, mongooseOptions, err => {
+  if (err) throw err;
+  console.log("database conncted!..");
+});
+//needed to listen to http (node server)insted of app.listen because of link of node to socket and express
+http.listen(port, () => {
+  console.log(`server is running on port :${port}`);
 });
